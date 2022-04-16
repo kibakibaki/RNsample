c88f693848354664e87857139d2b46ee9ca97620
kibakibaki <mrkibaki@gmail.com>
2022-04-16 17:06:28 +1000

    intro

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-This is a text
+This is a sample for COMP3130 Deliverable 1 coded by react-native
+The main idea is to develop one mobile app sample which is for learning mainly.
--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React from "react";
+import { View } from "react-native";
 
 function HomeScreen(props) {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <View style={styles.welcomeContainer}>
+      {/* 1. this doesn't aligned the icon for me to the centre when i put it in <ImageBackground> */}
+      <MaterialCommunityIcons
+        style={styles.welcomeContainer}
+        name="library"
+        size={100}
+        color={AppColors.otherColor}
+      />
+      <AppText>Welcome!</AppText>
+    </View>
+  );
 }
 
-const styles = StyleSheet.create({
-    
-})
+const styles = StyleSheet.create({});
 
 export default HomeScreen;
